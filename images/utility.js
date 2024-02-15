0bc4c8951ff0f5c5abde721157c43421c351f130
rakibulVSC <mrakibulhasan755@gmail.com>
2024-02-15 10:02:23 +0600

    add text

--- images/utility.js
+++ images/utility.js
@@ -7,10 +7,22 @@ function showElement(elementId) {
     element.classList.remove('hidden');
 }
 
-function setBackgroundColorById(elementId){
-const element=document.getElementById('elementId');
-element.classList.add('bg-orange-400');
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId); // Use the parameter instead of the string 'elementId'
+    element.classList.add('bg-orange-400'); // Use 'element' instead of 'Element', and fix the classList method
 }
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId); // Use the parameter instead of the string 'elementId'
+    element.classList.remove('bg-orange-400'); // Use 'element' instead of 'Element', and fix the classList method
+}
+
+function getElementValueById(elementId){
+ const element=document.getElementById('elementId');
+ const elementValueText=element.innerText;
+ const value=parseInt(elementValueText);
+ return value;
+}
+
 
 function getARandomAlphabet() {
     // get or create an alphabet array 
